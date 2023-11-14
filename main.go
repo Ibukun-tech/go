4ca570fe2f7a85ec844392a7bc60770b859fd70f
Ibukun-tech <oyetunjiibukunoluwa8@gmail.com>
2023-11-14 20:40:14 +0100

    Updating my first version

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	fmt.Println("I have been feeling very well")
+}
+
+func thinkingWell() string {
+	return "You are still learning and the best way to learn is by doingthe practical of what you did"
+}
