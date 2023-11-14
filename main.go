43f6f7f8a96acc63706aa9744ed29873d2074250
Ibukun-tech <oyetunjiibukunoluwa8@gmail.com>
2023-11-14 20:47:24 +0100

    Just updated it for others to use

--- main.go
+++ main.go
@@ -4,8 +4,21 @@ import "fmt"
 
 func main() {
 	fmt.Println("I have been feeling very well")
+	thinkingWell()
 }
 
 func thinkingWell() string {
 	return "You are still learning and the best way to learn is by doingthe practical of what you did"
+}
+func Math(value int, value1 int, s string) int {
+	switch {
+	case s == "+":
+		return value + value1
+	case s == "-":
+		return value - value1
+	}
+	return 0
+}
+func Food(s string) string {
+	return "This is the best food " + s
 }
